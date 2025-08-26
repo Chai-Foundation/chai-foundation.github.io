1f15283c415cf2d3218a47167b41b0b63d6d866b
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-08-26 13:22:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,41 @@
-// next.config.js
-module.exports = {
-  images: {
-    unoptimized: true,
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
-  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ui.aceternity.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  output: "export",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      type: 'asset/source',
+    })
+    return config
+  },
 };
+
+export default nextConfig;
